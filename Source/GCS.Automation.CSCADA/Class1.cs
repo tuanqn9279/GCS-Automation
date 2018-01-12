615fccbe33c483b5b5f61098bbed07851e4ce11d
tuanqn <tuanqn@gcs-vn.com>
2018-01-12 14:25:00 +0700

    Commit Init source code auto.

---
+++ Source/GCS.Automation.CSCADA/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCS.Automation.CSCADA
+{
+    public class Class1
+    {
+    }
+}
---
+++ Source/GCS.Automation.e2studio/Class1.cs
@@ -0,0 +1,12 @@
+﻿using PowerShell;
+
+namespace GCS.Automation.e2studio
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
